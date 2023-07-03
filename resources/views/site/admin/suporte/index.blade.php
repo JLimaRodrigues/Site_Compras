201e20c7fd4e8196cad4e47c580d8db2e57420c1
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2023-07-03 12:53:09 -0300

    criando-listagem-suportes

--- resources/views/site/admin/suporte/index.blade.php
+++ resources/views/site/admin/suporte/index.blade.php
@@ -1,1 +1,22 @@
-<h1>Listagem de Solicitações</h1
+<h1>Listagem de Solicitações</h1>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        @foreach($suportes as $suporte)
+            <tr>
+                <td>{{ $suporte->assunto }}</td>
+                <td>{{ $suporte->status }}</td>
+                <td>{{ $suporte->conteudo }}</td>
+                <td>  
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
